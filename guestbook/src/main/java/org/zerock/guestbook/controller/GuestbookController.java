bf4e85f8e9a01c7147eca4732bb4518b82b307fd
DEOKJAE KWON <juintin@kakao.com>
2024-02-17 17:23:54 +0900

    feat: framework of the project

---
+++ guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -0,0 +1,25 @@
+package org.zerock.guestbook.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/guestbook")
+@Log4j2
+public class GuestbookController {
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public String list() {
+
+        log.info("list.............");
+
+        return "guestbook/list";
+    }
+}
